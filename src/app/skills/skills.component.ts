4237a9a944d4ed559ab4b03e8afda781d248125e
Swetha S <85669283+swetha2662@users.noreply.github.com>
2023-03-15 12:06:14 +0530

    Update skills.component.ts

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,5 +1,4 @@
 import { Component,OnInit } from '@angular/core';
-import { Skill } from '../models/models';
 
 @Component({
   selector: 'app-skills',
@@ -7,24 +6,5 @@ import { Skill } from '../models/models';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
-  skills: Skill[] = [
-    {
-      name: 'Languages - C, C++, Python, Java',      
-    },
-    {
-      name: 'Web Development - HTML, CSS, JavaScript, ReactJS - beginner',
-    },
-    {
-      name: 'Web Frameworks - Angular, AngularJS',
-    },
-    {
-      name: 'Database - MySQL, PHP',
-    },
-    {
-      name: 'Data Structures and Algorithms',
-    },
-  ];
-  constructor() {}
-
   ngOnInit(): void {}
 }
